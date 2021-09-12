572dc36d87859045aabb15958a34ac04cc75446b
Tameem <timoooo99995@gmail.com>
2021-09-12 10:31:41 +0300

    Favorite page

--- lib/screens/favoritePage.dart
+++ lib/screens/favoritePage.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 
-
 class FavoritePage extends StatefulWidget {
   @override
   _FavoritePageState createState() => _FavoritePageState();
 }
 
 class _FavoritePageState extends State<FavoritePage> {
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: (
-        Container(
+        appBar: AppBar(
+          backgroundColor: Colors.teal[50],
+          elevation: 0,
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(),
+              child: Text(
+                "Favorite",
+                style: TextStyle(
+                  fontSize: 35.0,
+                  height: 1.3,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Center(
+              child: Icon(
+                Icons.favorite_border_outlined,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(width: 190.0),
+          ],
+        ),
+        body: (Container(
           color: Colors.teal[100],
-         
-       )
-
-    )
-
-);
-}
+        )));
+  }
 }
