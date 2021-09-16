605861ae66467b89661e831656997fb8158bed9b
Tameem <timoooo99995@gmail.com>
2021-09-16 13:25:57 +0300

    Add search box in card & search pages

--- lib/screens/cardPage.dart
+++ lib/screens/cardPage.dart
@@ -11,17 +11,17 @@ class _CardPageState extends State<CardPage> {
     return Scaffold(
       backgroundColor: Colors.teal[50],
       appBar: AppBar(
-         backgroundColor: Colors.teal[100],
-         shape: RoundedRectangleBorder(
-         borderRadius: BorderRadius.vertical(
-         bottom: Radius.circular(30),
-        ),
+        backgroundColor: Colors.teal[100],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
         ),
         elevation: 10,
         toolbarHeight: 90,
         title: Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             Text(
               'Cards  ',
               style: TextStyle(
@@ -31,12 +31,35 @@ class _CardPageState extends State<CardPage> {
             ),
             Icon(
               Icons.credit_card_rounded,
-              color: Color.fromARGB(240,74, 189, 159),
+              color: Color.fromARGB(240, 74, 189, 159),
               size: 35,
             ),
           ],
         ),
-      ),  
+      ),
+      body: Container(
+        constraints: BoxConstraints.tightFor(width: 500),
+        //width text field
+        margin: EdgeInsets.all(10),
+        child: Material(
+          // shadow
+          elevation: 5, //shadow
+          shadowColor: Colors.black, //color shadow
+          borderRadius: BorderRadius.circular(32.0),
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(8),
+              //size Text field
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+              labelText: "Search for the card",
+              labelStyle: TextStyle(fontSize: 20),
+              suffixIcon: Icon(Icons.credit_card_outlined),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -11,17 +11,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: Colors.teal[50],
+      backgroundColor: Colors.teal[50],
       appBar: AppBar(
-         backgroundColor: Colors.teal[100],
-         shape: RoundedRectangleBorder(
-         borderRadius: BorderRadius.vertical(
-         bottom: Radius.circular(30),
-      ),
-    ),
+        backgroundColor: Colors.teal[100],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
         elevation: 10,
         toolbarHeight: 70,
         title: Row(
@@ -36,14 +36,16 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             IconButton(
-              icon: const Icon(Icons.add_alert, color: Colors.black45,),          
-              onPressed: () {
-              },
-            ), 
+              icon: const Icon(
+                Icons.add_alert,
+                color: Colors.black45,
+              ),
+              onPressed: () {},
+            ),
           ],
         ),
       ),
-      body:Container(
+      body: Container(
         child: ListView(
           padding: EdgeInsets.only(left: 20.0),
           children: <Widget>[
@@ -58,86 +60,83 @@ class _HomePageState extends State<HomePage> {
             titlenearEvent(),
             SizedBox(height: 1.0),
             nearEventsList(),
-            SizedBox(height: 10.0),  
+            SizedBox(height: 10.0),
           ],
         ),
       ),
-     floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
-             Navigator.push(
-              context,
+          Navigator.push(
+            context,
             MaterialPageRoute(builder: (context) => AddEvent()),
-                );
+          );
         },
-        child: const Icon(Icons.add_outlined,size: 35,),
+        child: const Icon(
+          Icons.add_outlined,
+          size: 35,
+        ),
         backgroundColor: Colors.teal[200],
       ),
     );
   }
-  }
+}
 
- categoryList() {
-    return Container(
-      height: 60,
-      width: 50,
-        child: Categories()
-    );
-  }
+categoryList() {
+  return Container(height: 60, width: 50, child: Categories());
+}
 
-  titlenewEvent() {
-    return Row(
-      
-      children: <Widget>[
-        Text(
-          "New Event", 
-          style: TextStyle(
-            fontSize: 23,
-            fontWeight: FontWeight.w800,
-            color: Colors.black87,
-          ),
+titlenewEvent() {
+  return Row(
+    children: <Widget>[
+      Text(
+        "New Event",
+        style: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.w800,
+          color: Colors.black87,
         ),
-      ],
-    );
-  }
-
-  topEventsList() {
-    return Container(
-     
-      height: 275,
-      width: 300,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: events.length,
-        itemBuilder: (BuildContext context, int index) {
-          Map event = events[index];
-          return TopEvent(
-            event: event,
-          );
-        },
       ),
-    );
-  }
+    ],
+  );
+}
 
-  titlenearEvent() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text(
-          "Event near from you",
-          style: TextStyle(
-            fontSize: 23,
-            fontWeight: FontWeight.w800,
-            color: Colors.black87,
-          ),
+topEventsList() {
+  return Container(
+    height: 275,
+    width: 300,
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemCount: events.length,
+      itemBuilder: (BuildContext context, int index) {
+        Map event = events[index];
+        return TopEvent(
+          event: event,
+        );
+      },
+    ),
+  );
+}
+
+titlenearEvent() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Text(
+        "Event near from you",
+        style: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.w800,
+          color: Colors.black87,
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
+}
 
-  nearEventsList() {
-    return Container(
-      height:800.0,
+nearEventsList() {
+  return Container(
+      height: 800.0,
       margin: new EdgeInsets.symmetric(vertical: 2.0),
       child: ListView.builder(
         itemCount: events.length,
@@ -147,7 +146,5 @@ class _HomePageState extends State<HomePage> {
             event: event,
           );
         },
-      )
-    );
-  }
-
+      ));
+}
